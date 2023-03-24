6cb39ec1d2413fe11673482d374d8c446dde0422
Kshishtof <wessya@gmail.com>
2023-03-24 22:15:16 +1000

    V00

---
+++ Warp_6/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Warp_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Go = new System.Windows.Forms.RadioButton();
+            this.Power = new System.Windows.Forms.RadioButton();
+            this.MorePower = new System.Windows.Forms.Button();
+            this.LessPower = new System.Windows.Forms.Button();
+            this.ShowPower = new System.Windows.Forms.TextBox();
+            this.Ship1 = new System.Windows.Forms.RadioButton();
+            this.Ship2 = new System.Windows.Forms.RadioButton();
+            this.Ship3 = new System.Windows.Forms.RadioButton();
+            this.Ship6 = new System.Windows.Forms.RadioButton();
+            this.Ship5 = new System.Windows.Forms.RadioButton();
+            this.Ship4 = new System.Windows.Forms.RadioButton();
+            this.Ship9 = new System.Windows.Forms.RadioButton();
+            this.Ship8 = new System.Windows.Forms.RadioButton();
+            this.Ship7 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start.Location = new System.Drawing.Point(224, 642);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(114, 46);
+            this.start.TabIndex = 0;
+            this.start.Text = "Ход!";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(511, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1499, 1080);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Go
+            // 
+            this.Go.AutoSize = true;
+            this.Go.Location = new System.Drawing.Point(20, 52);
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(286, 43);
+            this.Go.TabIndex = 2;
+            this.Go.TabStop = true;
+            this.Go.Text = "Передвижение ";
+            this.Go.UseVisualStyleBackColor = true;
+            this.Go.CheckedChanged += new System.EventHandler(this.Go_CheckedChanged);
+            // 
+            // Power
+            // 
+            this.Power.AutoSize = true;
+            this.Power.Location = new System.Drawing.Point(20, 101);
+            this.Power.Name = "Power";
+            this.Power.Size = new System.Drawing.Size(202, 43);
+            this.Power.TabIndex = 3;
+            this.Power.TabStop = true;
+            this.Power.Text = "Мощность";
+            this.Power.UseVisualStyleBackColor = true;
+            this.Power.CheckedChanged += new System.EventHandler(this.Power_CheckedChanged);
+            // 
+            // MorePower
+            // 
+            this.MorePower.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MorePower.Location = new System.Drawing.Point(418, 481);
+            this.MorePower.Name = "MorePower";
+            this.MorePower.Size = new System.Drawing.Size(42, 46);
+            this.MorePower.TabIndex = 4;
+            this.MorePower.Text = ">";
+            this.MorePower.UseVisualStyleBackColor = true;
+            // 
+            // LessPower
+            // 
+            this.LessPower.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LessPower.Location = new System.Drawing.Point(360, 482);
+            this.LessPower.Name = "LessPower";
+            this.LessPower.Size = new System.Drawing.Size(42, 45);
+            this.LessPower.TabIndex = 5;
+            this.LessPower.Text = "<";
+            this.LessPower.UseVisualStyleBackColor = true;
+            // 
+            // ShowPower
+            // 
+            this.ShowPower.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ShowPower.Location = new System.Drawing.Point(360, 431);
+            this.ShowPower.Name = "ShowPower";
+            this.ShowPower.Size = new System.Drawing.Size(100, 45);
+            this.ShowPower.TabIndex = 6;
+            // 
+            // Ship1
+            // 
+            this.Ship1.AutoSize = true;
+            this.Ship1.Location = new System.Drawing.Point(41, 64);
+            this.Ship1.Name = "Ship1";
+            this.Ship1.Size = new System.Drawing.Size(57, 43);
+            this.Ship1.TabIndex = 8;
+            this.Ship1.TabStop = true;
+            this.Ship1.Text = "1";
+            this.Ship1.UseVisualStyleBackColor = true;
+            // 
+            // Ship2
+            // 
+            this.Ship2.AutoSize = true;
+            this.Ship2.Location = new System.Drawing.Point(41, 113);
+            this.Ship2.Name = "Ship2";
+            this.Ship2.Size = new System.Drawing.Size(57, 43);
+            this.Ship2.TabIndex = 9;
+            this.Ship2.TabStop = true;
+            this.Ship2.Text = "2";
+            this.Ship2.UseVisualStyleBackColor = true;
+            // 
+            // Ship3
+            // 
+            this.Ship3.AutoSize = true;
+            this.Ship3.Location = new System.Drawing.Point(41, 162);
+            this.Ship3.Name = "Ship3";
+            this.Ship3.Size = new System.Drawing.Size(57, 43);
+            this.Ship3.TabIndex = 10;
+            this.Ship3.TabStop = true;
+            this.Ship3.Text = "3";
+            this.Ship3.UseVisualStyleBackColor = true;
+            // 
+            // Ship6
+            // 
+            this.Ship6.AutoSize = true;
+            this.Ship6.Location = new System.Drawing.Point(122, 162);
+            this.Ship6.Name = "Ship6";
+            this.Ship6.Size = new System.Drawing.Size(57, 43);
+            this.Ship6.TabIndex = 13;
+            this.Ship6.TabStop = true;
+            this.Ship6.Text = "6";
+            this.Ship6.UseVisualStyleBackColor = true;
+            // 
+            // Ship5
+            // 
+            this.Ship5.AutoSize = true;
+            this.Ship5.Location = new System.Drawing.Point(122, 113);
+            this.Ship5.Name = "Ship5";
+            this.Ship5.Size = new System.Drawing.Size(57, 43);
+            this.Ship5.TabIndex = 12;
+            this.Ship5.TabStop = true;
+            this.Ship5.Text = "5";
+            this.Ship5.UseVisualStyleBackColor = true;
+            // 
+            // Ship4
+            // 
+            this.Ship4.AutoSize = true;
+            this.Ship4.Location = new System.Drawing.Point(122, 64);
+            this.Ship4.Name = "Ship4";
+            this.Ship4.Size = new System.Drawing.Size(57, 43);
+            this.Ship4.TabIndex = 11;
+            this.Ship4.TabStop = true;
+            this.Ship4.Text = "4";
+            this.Ship4.UseVisualStyleBackColor = true;
+            // 
+            // Ship9
+            // 
+            this.Ship9.AutoSize = true;
+            this.Ship9.Location = new System.Drawing.Point(196, 162);
+            this.Ship9.Name = "Ship9";
+            this.Ship9.Size = new System.Drawing.Size(57, 43);
+            this.Ship9.TabIndex = 16;
+            this.Ship9.TabStop = true;
+            this.Ship9.Text = "9";
+            this.Ship9.UseVisualStyleBackColor = true;
+            // 
+            // Ship8
+            // 
+            this.Ship8.AutoSize = true;
+            this.Ship8.Location = new System.Drawing.Point(196, 113);
+            this.Ship8.Name = "Ship8";
+            this.Ship8.Size = new System.Drawing.Size(57, 43);
+            this.Ship8.TabIndex = 15;
+            this.Ship8.TabStop = true;
+            this.Ship8.Text = "8";
+            this.Ship8.UseVisualStyleBackColor = true;
+            // 
+            // Ship7
+            // 
+            this.Ship7.AutoSize = true;
+            this.Ship7.Location = new System.Drawing.Point(196, 64);
+            this.Ship7.Name = "Ship7";
+            this.Ship7.Size = new System.Drawing.Size(57, 43);
+            this.Ship7.TabIndex = 14;
+            this.Ship7.TabStop = true;
+            this.Ship7.Text = "7";
+            this.Ship7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Power);
+            this.groupBox1.Controls.Add(this.Go);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 414);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(315, 150);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор дейстивя";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Ship9);
+            this.groupBox2.Controls.Add(this.Ship8);
+            this.groupBox2.Controls.Add(this.Ship7);
+            this.groupBox2.Controls.Add(this.Ship6);
+            this.groupBox2.Controls.Add(this.Ship5);
+            this.groupBox2.Controls.Add(this.Ship4);
+            this.groupBox2.Controls.Add(this.Ship3);
+            this.groupBox2.Controls.Add(this.Ship2);
+            this.groupBox2.Controls.Add(this.Ship1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(139, 150);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 231);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Корабли";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ShowPower);
+            this.Controls.Add(this.LessPower);
+            this.Controls.Add(this.MorePower);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RadioButton Go;
+        private System.Windows.Forms.RadioButton Power;
+        private System.Windows.Forms.Button MorePower;
+        private System.Windows.Forms.Button LessPower;
+        private System.Windows.Forms.TextBox ShowPower;
+        private System.Windows.Forms.RadioButton Ship1;
+        private System.Windows.Forms.RadioButton Ship2;
+        private System.Windows.Forms.RadioButton Ship3;
+        private System.Windows.Forms.RadioButton Ship6;
+        private System.Windows.Forms.RadioButton Ship5;
+        private System.Windows.Forms.RadioButton Ship4;
+        private System.Windows.Forms.RadioButton Ship9;
+        private System.Windows.Forms.RadioButton Ship8;
+        private System.Windows.Forms.RadioButton Ship7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Warp_6/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+
+namespace Warp_6
+{
+    struct Position
+    {
+        public double pi;
+        public double x;
+        public double y;
+        public int jump;
+        public bool busy;
+    }
+
+    struct Addres_Dash
+    {
+        public int _external;
+        public int _internal;
+    }
+
+    struct Ship
+    {
+        int position;
+        int type;
+        int power;
+
+
+    }
+
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Graphics graph_bitmap;
+        Bitmap bitmap;
+        Position[] position = new Position[126];
+        Addres_Dash[] addres_dash = new Addres_Dash[30];
+        short i = 126;
+
+
+        SolidBrush BrushBlack = new SolidBrush(Color.Black);
+        SolidBrush BrushGold = new SolidBrush(Color.Gold);
+        SolidBrush BrushLimeGreen = new SolidBrush(Color.LimeGreen);
+        Pen BlackPen = new Pen(Color.Black, 3);
+        
+
+        const int k = 10;
+        const double turn = 3.11;
+
+        double x;
+        double y;
+
+        void DrawDashLine(int a, int b)
+        {
+            graph_bitmap.DrawLine(BlackPen, (float)position[a].x, (float)position[a].y, (float)position[b].x, (float)position[b].y); 
+        }
+        void DrawCircle(double x, double y, float radius)
+        {
+            graph_bitmap.FillEllipse(BrushBlack, (float)(x - (radius / 2)), (float)(y - (radius / 2)), radius, radius);
+        }
+        void DrawTriangle(SolidBrush brush, int index)
+        {
+            String drawString = "3";
+            Font drawFont = new Font("Arial", 25);
+            float radius = 30;
+            float x = (float)position[index].x;
+            float y = (float)position[index].y;
+            float Shift = (float)Math.Sqrt(Math.Pow(radius, 2) - Math.Pow(radius / 2, 2));
+            PointF TopPoint = new PointF(x, y - radius);
+            PointF BottomLeftPoint = new PointF(x - Shift, y + (radius / 2));
+            PointF BottomRightPoint = new PointF(x + Shift, y + (radius / 2));
+            PointF[] curvePoints = { TopPoint, BottomLeftPoint, BottomRightPoint };
+            graph_bitmap.FillPolygon(brush, curvePoints);
+            graph_bitmap.DrawString(drawString, drawFont, BrushBlack, x - (radius / 3)-4, y - (radius / 2)-4);
+            graphics.DrawImage(bitmap, 0, 0, pictureBox1.Size.Width, pictureBox1.Size.Height);
+        }
+        
+        void DrawRectangle(SolidBrush brush, int index)
+        {
+            String drawString = "3";
+            Font drawFont = new Font("Arial", 25);
+            float radius = 40;
+            float x = (float)position[index].x;
+            float y = (float)position[index].y;
+            graph_bitmap.FillRectangle(brush, x - (radius / 2), y - (radius / 2), radius, radius);
+            graph_bitmap.DrawString(drawString, drawFont, BrushBlack, x - (radius / 3) - 1, y - (radius / 2) + 2);
+            graphics.DrawImage(bitmap, 0, 0, pictureBox1.Size.Width, pictureBox1.Size.Height);
+        }
+        void DrawCircle(SolidBrush brush, int index)
+        {
+            String drawString = "3";
+            Font drawFont = new Font("Arial", 25);
+            float radius = 40;
+            float x = (float)position[index].x;
+            float y = (float)position[index].y;
+            graph_bitmap.FillEllipse(brush, x - (radius / 2), y - (radius / 2), radius, radius);
+            graph_bitmap.DrawString(drawString, drawFont, BrushBlack, x - (radius / 3)-1, y - (radius / 2)+2);
+            graphics.DrawImage(bitmap, 0, 0, pictureBox1.Size.Width, pictureBox1.Size.Height);
+        }
+
+        double PolarToX(double pi, double turn, int k)
+        {
+            double p = pi * k;
+            double x = p * Math.Cos(pi + turn) + pictureBox1.Size.Width / 2; ;
+            return x;
+        }
+
+        double PolarToY(double pi, double turn, int k)
+        {
+            double p = pi * k;
+            double y = p * Math.Sin(pi + turn) + pictureBox1.Size.Height / 2 - 25;
+            return y;
+        }
+
+        void Map()
+        {
+            double pi = 0;
+            float radius;
+            radius = 40;
+            
+
+            graph_bitmap.Clear(Color.White);
+            
+            BlackPen.DashStyle = DashStyle.Dash;
+
+            DrawCircle(pictureBox1.Size.Width / 2, pictureBox1.Size.Height / 2 - 25, radius);
+
+            radius = 4;
+            //Отрисовывание сприрали 
+            for (int i = 0; i < 41400; i++)
+            {
+                x = PolarToX(pi, turn, k);
+                y = PolarToY(pi, turn, k);
+                DrawCircle(x, y, radius);
+                pi += 0.001;
+            }
+
+            //Отрисовывание точек
+            radius = 25;
+
+            for (int i = 0; i < 126; i++)
+            {
+                DrawCircle(position[i].x, position[i].y, radius);
+            }
+
+            DrawDashLine(90, 108);
+            DrawDashLine(96, 114);
+            DrawDashLine(102, 120);
+
+            for (int i = 0; i < 30; i++)
+            {
+                DrawDashLine(addres_dash[i]._external, addres_dash[i]._internal);
+            }
+
+            graphics.DrawImage(bitmap, 0, 0, pictureBox1.Size.Width, pictureBox1.Size.Height);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            Map();
+            if (i != 3) { i--; }
+            else { i = 125; };
+            DrawTriangle(BrushLimeGreen, i);
+            DrawRectangle(BrushLimeGreen, i - 1);
+            DrawCircle(BrushLimeGreen, i - 2);
+            DrawCircle(BrushGold, i - 3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitmap = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+            graph_bitmap = Graphics.FromImage(bitmap);
+            graphics = pictureBox1.CreateGraphics();
+            int[] index = new int[6];
+            short count;
+            short dest = 6;
+            double pi;
+            double start_angle;
+            double step;
+
+            for (short i = 0; i < 6; i++)
+            {
+                position[i].jump = -1;
+                index[i] = i;
+            }
+
+            count = 6;
+            //Запоминатие прыжков
+            for (int i = 2; i <= 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 2; k++)
+                    {
+                        position[count].jump = index[j];
+                        count++;
+                    }
+
+                    index[j]++;
+                    for (int k = 2; k < i; k++)
+                    {
+                        position[count].jump = index[j];
+                        index[j]++;
+                        count++;
+                    }
+                }
+                index[0] = index[5];
+                for (int j = 1; j < 6; j++)
+                {
+                    index[j] = index[j - 1] + i;
+                }
+            }
+            //Запоминание координат точек
+            pi = 3.7;
+            start_angle = 1.047;
+            count = 0;
+            for (int i = 1; i <= 6; i++)
+            {
+                step = start_angle;
+                step = step / i;
+                for (int j = 0; j < 6 * i; j++)
+                {
+                    x = PolarToX(pi, turn, k);
+                    y = PolarToY(pi, turn, k);
+                    position[count].pi = pi;
+                    position[count].x = x;
+                    position[count].y = y;
+                    position[count].busy = false;
+                    count++;
+                    pi += step;
+                }
+            }
+
+            //Запоминание координат пунктирных линий
+            addres_dash[0]._external = 95;
+            addres_dash[0]._internal = 0;
+
+            for (int i = 1; i < 5; i++)
+            {
+                addres_dash[i]._external = addres_dash[i-1]._external - 1;
+                addres_dash[i]._internal = addres_dash[i-1]._internal + dest * i;
+            }
+
+            count = 5;
+
+            for (short i = 0; i < 5; i++)
+            {
+                for (short j = 1; j < 6; j++)
+                {
+                    addres_dash[count]._external = addres_dash[count - 5]._external + dest;
+                    addres_dash[count]._internal = addres_dash[count - 5]._internal + j;
+                    count++;
+                }
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Power_CheckedChanged(object sender, EventArgs e)
+        {
+            ShowPower.ReadOnly = false;
+        }
+
+        private void Go_CheckedChanged(object sender, EventArgs e)
+        {
+            ShowPower.ReadOnly = true;
+        }
+    }
+}
