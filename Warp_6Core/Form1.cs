8db8a30e3f7b4a5ba51ea1d3cbdfe3724c8e7dfc
SirKshishtof <wessya@gmail.com>
2023-10-31 17:01:30 +1000

    Тест переноса(Удаление закоментированных библеотек)

--- Warp_6Core/Form1.cs
+++ Warp_6Core/Form1.cs
@@ -1,19 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Reflection;
-//using System.Linq;
-//using System.Security.Cryptography;
-//using System.Text;
 using System.Threading;
-//using System.Threading.Tasks;
 using System.Windows.Forms;
-//using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-//using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 
 namespace Warp_6
